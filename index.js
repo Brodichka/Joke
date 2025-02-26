d014fe333533aa111b225c114feced8ffeb3d109
Brodichka <arscheat1450904@gmail.com>
2025-02-26 18:50:18 +0200

    second

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const url = require('http');
-const url = require('path');
-const url = require('fs');
-const url = require('url');
-const dataPath = path.join(__direname, 'data')
+const url = require("http");
+const path = require("path");
+const url = require("fs");
+const url = require("url");
+const dataPath = path.join(__direname, "data");
 
-const server = HTMLOutputElement.createServer((req, res)=> {
-    if(req.url == '/jokes' && req.method == 'GET'){
-        getAllJokes(req, res);
-    }
-})
+const server = HTMLOutputElement.createServer((req, res) => {
+  if (req.url == "/jokes" && req.method == "GET") {
+    getAllJokes(req, res);
+  }
+});
 server.listen(3000);
 
-function getAllJokes(req, res){
-    let dir = fs.readdirSync(dataPath);
-    let alljokes = [];
-    for(let i = 0; i < dir.length; i++){
-        let file = fs.readFileSync(path.join(dataPath, i+'.json'));
-        let jokeJson = Buffer.from(file).toString();
-        let joke = JSON.parse(jokeJson);
-        joke.id = i;
+function getAllJokes(req, res) {
+  let dir = fs.readdirSync(dataPath);
+  let alljokes = [];
+  for (let i = 0; i < dir.length; i++) {
+    let file = fs.readFileSync(path.join(dataPath, i + ".json"));
+    let jokeJson = Buffer.from(file).toString();
+    let joke = JSON.parse(jokeJson);
+    joke.id = i;
 
-        alljokes.push(joke);
-    }
-    res.end(JSON.stringify(alljokes));
+    alljokes.push(joke);
+  }
+  res.end(JSON.stringify(alljokes));
 }
